08adef69fd9295c8239b8fd4d17171a7ee663677
NexusBill <nexusbills.official@gmail.com>
2025-11-29 09:00:40 +0530

    multi tenant based

--- server.js
+++ server.js
@@ -494,7 +494,15 @@ const customerRoutes = require("./controller/customerRoutes");
 
 app.use(express.json());
 app.use("/api", tenantRoutes);
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 // multitenant path route
 app.use("/api/:clientCode/products", productRoutes);
 app.use("/api/:clientCode/categories", categoryRoute);
